817ddc232749c1a92734b701a6f540b723bec537
LondheShubham153 <shubhamnath5@gmail.com>
2023-05-13 23:53:23 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Serverless!",
+        message: "Hello From TWS Serverless!",
       },
       null,
       2
