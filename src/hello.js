837e95651a7acf2ec11e99ce5c5b53c26b956530
Shubham Londhe <shubhamnath5@gmail.com>
2023-09-23 12:09:19 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From TWS Batch 4!",
+        message: "Hello From TWS Batch 4 - You all are awesome!",
       },
       null,
       2
