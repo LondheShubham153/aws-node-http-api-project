267fd4cbec28d4c313324027b0676892b784ef4a
krishna078 <58102536+krishna078@users.noreply.github.com>
2023-09-24 21:24:02 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From TWS Batch 4 - You all are awesome!",
+        message: "Hello From TWS Batch 4 - we all are awesome!",
       },
       null,
       2
