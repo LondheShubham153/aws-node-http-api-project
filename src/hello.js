e4e5df36ca809bae4e8dcdc4090cc75ad112e292
Surajit Banerjee <139263134+surapkp@users.noreply.github.com>
2023-09-29 23:18:02 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From TWS Batch Foure - You all are great!",
+        message: "Hello From TWS Batch Foure - You all are great & awesome!",
       },
       null,
       2
